13d028c9a427841634cdfddb2e697c1560952858
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2025-06-15 16:40:22 +0200

    Added compatibility to TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.4.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-13.9.99',
             'lib_ical' => '0.5.0-0.5.99',
             'eventnews' => '6.0.0-7.99'
         ],
